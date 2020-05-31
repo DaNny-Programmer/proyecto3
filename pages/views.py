691c607cff07c1f43869d9d1ce445a0c2b7d7e34
Daniela Muñoz <daniimhdez08@gmail.com>
2020-05-30 22:38:25 -0500

    Seguridad V64 min 7.15

--- pages/views.py
+++ pages/views.py
@@ -7,9 +7,15 @@ from .models import Page
 from .forms import PageForm
 
 class StaffRequiredMixin(object):
-        """
+    """
     Este mixin requerirá que el usuario sea miembro del staff
     """
+        def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_staff:
+            return redirect(reverse_lazy('admin:login'))
+
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
+
 # Create your views here.
 class PageListView(ListView):
     model = Page
@@ -20,17 +26,10 @@ class PageDetailView(DetailView):
     model = Page
 
 
-class PageCreate(CreateView):
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
-
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
-
-        return super(PageCreate, self).dispatch(request, *args, **kwargs)
-
 
 class PageUpdate(UpdateView):
     model = Page
