fc7565a88708bc5234484d7670962ddeb980a645
Daniela Muñoz <daniimhdez08@gmail.com>
2020-05-30 23:08:08 -0500

    Seguridad  V64 terminado

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,17 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
 from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
 
+class StaffRequiredMixin(object):
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        return super(StaffRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 # Create your views here.
 class PageListView(ListView):
@@ -16,19 +22,13 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
-
-class PageCreate(CreateView):
+@method_decorator(staff_member_required, name='dispatch')
+class PageCreate(StaffRequiredMixin, CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
-    def dispatch(self, request, *args, **kwargs):
-        if not request.user.is_staff:
-            return redirect(reverse_lazy('admin:login'))
-
-        return super(PageCreate, self).dispatch(request, *args, **kwargs)
-
-
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
@@ -37,6 +37,7 @@ class PageUpdate(UpdateView):
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
