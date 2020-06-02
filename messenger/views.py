1e9362b2c793cc45bceea578dd3c19b65349d024
Daniela Muñoz <daniimhdez08@gmail.com>
2020-06-01 23:38:45 -0500

    Mensajes asincronos con JS V90

--- messenger/views.py
+++ messenger/views.py
@@ -1,12 +1,10 @@
-from django.shortcuts import render
-from django.views.generic.list import ListView
-from django.views.generic.detail import DetailView
-from django.views.generic import TemplateView
-from .models import Thread
-from django.http import Http404
-
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.views.generic.detail import DetailView
+from django.views.generic import TemplateView
+from django.http import Http404, JsonResponse
+from .models import Thread 
+
 
 # Create your views here.
 @method_decorator(login_required, name="dispatch")
@@ -22,3 +20,8 @@ class ThreadDetail(DetailView):
         if self.request.user not in obj.users.all():
             raise Http404()
         return obj
+    
+def add_message(request, pk):
+    print(request.GET)
+    json_response = {'created':False}
+    return JsonResponse(json_response)
